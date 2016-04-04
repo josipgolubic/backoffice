96662af0706c254e8dbfd643f22f2e14c3540932
josipgolubic <josipgolubic@gmail.com>
2016-04-04 11:59:36 +0100

    Db2

--- app/Http/Controllers/DBController.php
+++ app/Http/Controllers/DBController.php
@@ -9,6 +9,7 @@ class WelcomeController extends Controller {
 	    // Make call to application...
 
 	    $results = DB::select('select * from users', array(10));
+	    echo $results;
 	    //view('welcome', compact('results'));
 	}
 
