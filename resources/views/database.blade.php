08b43382387cddaaa0ead7ceb347362dcea595c9
josipgolubic <josipgolubic@gmail.com>
2016-04-04 15:06:33 +0100

    No layout extending.

--- resources/views/database.blade.php
+++ resources/views/database.blade.php
@@ -1,9 +1,18 @@
-@extends('layout')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
 
-@section('content')
+<body>
+
 	@foreach ($users as $user)
 		<div>
 			{{ $user->name; }}
 		</div>
 	@endforeach
-@stop
+
+</body>
+</html>
