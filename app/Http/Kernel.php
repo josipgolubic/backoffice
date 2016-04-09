48b14a09fccb508682c8817f500d9e0176f2fede
josipgolubic <josipgolubic@gmail.com>
2016-04-09 18:28:05 +0100

    Display dummy data

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends HttpKernel {
 		'auth' => 'App\Http\Middleware\Authenticate',
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
 		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
-		'cors' => App\Http\Middleware\Cors::class,
+		'cors' => 'App\Http\Middleware\CORS',
 	];
 
 }
