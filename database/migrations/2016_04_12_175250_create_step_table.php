091fa0c693ebe3085ae7174de79a3fe185ea3bcf
josipgolubic <josipgolubic@gmail.com>
2016-04-12 21:44:38 +0100

    Added migration files

---
+++ database/migrations/2016_04_12_175250_create_step_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStepTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('TABLE_STEP', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('KEY_NODE_ID')->unique();
+			$table->integer('KEY_NEIGHBOR_ID');
+			$table->integer('KEY_PATH_COST');
+			$table->integer('KEY_STEP_ORDER');
+			$table->string('KEY_STEP_IMAGE');
+			$table->string('KEY_STEP_DESCRIPTTION');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('TABLE_STEP');
+	}
+
+}
