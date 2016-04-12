ada4c9e3b6f60e884ada2a58e10192d614536bb4
josipgolubic <josipgolubic@gmail.com>
2016-04-12 21:01:37 +0100

    Created table migrations.

---
+++ database/migrations/2016_04_12_175207_create_node_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNodeTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('TABLE_NODE', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('KEY_NODE_ID');
+			$table->integer('KEY_XML_INFO_ID');
+			$table->string('KEY_BUILDING_DESCRIPTION');
+			$table->integer('KEY_FLOOR_LEVEL');
+			$table->string('KEY_FLOOR_DESCRIPTION');
+			$table->integer('KEY_FLOOR_LENGTH');
+			$table->integer('KEY_FLOOR_WIDTH');
+			$table->string('KEY_NODE_DESCRIPTION');
+			$table->integer('KEY_NODE_X_POSITION');
+			$table->integer('KEY_NODE_Y_POSTIION');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('TABLE_NODE');
+	}
+
+}
