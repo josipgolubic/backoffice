8c04c32b266f2824e0495e591d94622a6766d006
josipgolubic <josipgolubic@gmail.com>
2016-04-30 21:39:41 +0100

    Commit.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+				<div class="panel-heading">Add New</div>
 
 				<div class="panel-body">
-					You are logged in!
+					<a href="{{ URL::route('users.index'); }}">My button</a>
 				</div>
 			</div>
 		</div>
