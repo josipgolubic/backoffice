2e2592b55e54dbb58212837354cbd7796aefdb19
josipgolubic <josipgolubic@gmail.com>
2016-04-30 22:43:37 +0100

    Commit.

--- resources/views/forms/map.blade.php
+++ resources/views/forms/map.blade.php
@@ -1,19 +1,23 @@
 @extends('layout')
 
 @section('content')
-	<h3>Add a New Map</h3>
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<h3>Add a New Map</h3>
 
-	<form method="POST" action="/map/1">
-		<div class="form-group">
-			Map version:<input type="text" name="MapVersion"></input>
-			
-		</div>
-		<div class="form-group">
-			Map description:<input type="text" name="MapDescription"></input>
-		</div>
+			<form method="POST" action="/map/1">
+				<div class="form-group">
+					Map version:<input type="text" name="MapVersion"></input>
+					
+				</div>
+				<div class="form-group">
+					Map description:<input type="text" name="MapDescription"></input>
+				</div>
 
-		<div class="form-group">
-			<button type="submit" class="btn btn-primary">Add Map</button>
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary">Add Map</button>
+				</div>
+			</form>
 		</div>
-	</form>
+	</div>
 @stop
