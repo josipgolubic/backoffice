4c502fca34f0daa41ded60d6001c573ebb8b32a0
josipgolubic <josipgolubic@gmail.com>
2016-05-13 15:48:06 +0100

    Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,17 @@
 		<div class="container">
 			<div class="content">
 				<div class="title">Backoffice</div>
-				<a href="/maps" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Maps</span>
+				<ul class="list-inline intro-social-buttons">
+                    <li>
+                        <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                    </li>
+                    <li>
+                        <a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                    </li>
+                </ul>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
