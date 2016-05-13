2b6a084a5789f7fa7e7f0db1ae1ff8aedfe507bf
josipgolubic <josipgolubic@gmail.com>
2016-05-13 15:42:40 +0100

    Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,14 @@
 		<title>Laravel</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+		<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 		<link href="{{ asset('/css/landing.css') }}" rel="stylesheet">
 	</head>
 	<body>
 		<div class="container">
 			<div class="content">
 				<div class="title">Backoffice</div>
-				<a href="/maps" class="btn btn-default btn-lg">Maps</a>
+				<a href="/maps" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Maps</span>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
