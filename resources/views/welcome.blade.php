23afa1e921cd6a9599f367d0d1d54ab956730f21
josipgolubic <josipgolubic@gmail.com>
2016-05-13 15:37:08 +0100

    Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,77 +3,13 @@
 		<title>Laravel</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
-		<style>
-			.full {
-			    background: url('/images/backoffice.jpeg') no-repeat center center fixed;
-			    -webkit-background-size: cover;
-			    -moz-background-size: cover;
-			    background-size: cover;
-			    -o-background-size: cover;
-			}
-
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
-
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				font-weight: 900;
-    			text-shadow: 2px 2px 3px rgba(0,0,0,0.6)
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-
-			.btn {
-				display: inline-block;
-			  	margin-bottom: 0;
-			  	font-weight: 300;
-			  	text-align: center;
-			  	vertical-align: middle;
-			    -ms-touch-action: manipulation;
-			    	touch-action: manipulation;
-			  	cursor: pointer;
-			  	background-image: none;
-			  	border: 1px solid transparent;
-			  	white-space: nowrap;
-			  	padding: 6px 12px;
-			  	font-size: 14px;
-			  	line-height: 1.42857143;
-			  	border-radius: 4px;
-			    -webkit-user-select: none;
-			    -moz-user-select: none;
-			    -ms-user-select: none;
-			  	user-select: none;
-			}			
-		</style>
+		<link href="{{ asset('/css/landing.css') }}" rel="stylesheet">
 	</head>
 	<body>
 		<div class="container">
 			<div class="content">
 				<div class="title">Backoffice</div>
-					<a href="/maps" class="btn btn-default btn-lg">Maps</a>
+				<a href="/maps" class="btn btn-default btn-lg">Maps</a>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
