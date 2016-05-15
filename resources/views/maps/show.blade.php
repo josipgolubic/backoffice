301d1cd392a6c9818a7c9a698dd8585689ada159
josipgolubic <josipgolubic@gmail.com>
2016-05-15 01:32:39 +0100

    Commit.

--- resources/views/maps/show.blade.php
+++ resources/views/maps/show.blade.php
@@ -6,20 +6,24 @@
 			<div class="panel panel-default">
 				<div class="panel-heading">Map Overview</div>
 				<div class="panel-body">
-					<h2>Map: <small>{{ $map->description }}</small></h2>
+					<h3>Map: <small>{{ $map->description }}</small></h3>
 					<hr>
-					<h3>Map Version: <small>{{ $map->version }}</small></h3>
+					<h5>Map Version: {{ $map->version }}</h5>
+					<h5>Map Nodes:</h5>
 					<ul class="list-group">
 						@foreach ($map->nodes as $node)
 							<li class="list-group-item">{{ $node->description }}</li>
 						@endforeach
 					</ul>
+
 					<a class="btn btn-warning btn-block" href="edit/{{ $map->id }}">Edit</a>
+					<hr>
 					<form method="POST" action="../deletemap/{{ $map->id }}">
 					    <input type="hidden" name="_method" value="DELETE">
 						<button type="submit" class="btn btn-danger btn-block">Delete</button>
 						<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
 					</form>
+					<hr>
 					<a class="btn btn-info btn-block" href="{{ url('/maps') }}">Go Back</a>
 				</div>
 				<!-- /.panel-body -->
