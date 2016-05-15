c1633642760fb713ce7099e75725393b8ab55ab7
josipgolubic <josipgolubic@gmail.com>
2016-05-15 01:50:51 +0100

    Commit.

--- resources/views/maps/show.blade.php
+++ resources/views/maps/show.blade.php
@@ -2,7 +2,7 @@
 	
 @section('content')
 	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
+		<div class="col-md-6 col-md-offset-3">
 			<div class="panel panel-default">
 				<div class="panel-heading">Map Overview</div>
 				<div class="panel-body">
