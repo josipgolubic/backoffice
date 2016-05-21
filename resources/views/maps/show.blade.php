25c1774abfedc80536759a84aff3e02f3df4ffc1
josipgolubic <josipgolubic@gmail.com>
2016-05-21 20:59:46 +0100

    Commit.

--- resources/views/maps/show.blade.php
+++ resources/views/maps/show.blade.php
@@ -27,7 +27,7 @@
 
 					<button type="button" class="btn btn-default btn-block" data-toggle="collapse" data-target="#addnode"><strong>Add New Node</strong></button>
 				  	<div id="addnode" class="collapse">
-						@include('app')
+						@include('nodes.show')
 					</div>
 					<!-- /.collapse id=addnode -->
 
